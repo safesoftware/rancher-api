8c6b7b2495ad9e86ab5a986091eecfbf4decbcc8
Mordecai Hecht <mordecai.hecht@safe.com>
2017-08-15 09:51:21 -0700

    Use the project specific stack url.

--- lib/rancher/api/models/stack.rb
+++ lib/rancher/api/models/stack.rb
@@ -4,6 +4,8 @@ module Rancher
       include Her::Model
       include Helpers::Model
 
+      collection_path '/v2-beta/projects/:project_id/stacks'
+
       belongs_to :project
       has_many :services
     end
